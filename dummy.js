4bfa8da8931522127c1265fe87dd8b328cc1ac78
Damien Rochford <djrochford@gmail.com>
2016-09-23 17:08:25 -0700

    Creates folders for each user, does not assign html correctly

--- dummy.js
+++ dummy.js
@@ -17,29 +17,29 @@ dummy.get('/links/:userid', function(req, res) {
   }
 
   linksObject = {
-    ownlinks: {
-      '1': ['http://www.nytimes.com/2016/09/23/us/charlotte-image-police-shooting.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=b-lede-package-region&region=top-news&WT.nav=top-news&_r=0',
+    ownLinks: {
+      '1%Damien': ['http://www.nytimes.com/2016/09/23/us/charlotte-image-police-shooting.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=b-lede-package-region&region=top-news&WT.nav=top-news&_r=0',
         'http://www.vox.com/policy-and-politics/2016/9/23/13001830/donald-trump-jr-tweets-controversy-alt-right-white-nationalism',
         'http://www.slate.com/articles/double_x/doublex/2016/09/trump_induced_anxiety_is_a_real_thing.html'
       ],
-      '2': [
+      '2%Jordan': [
         'https://www.theguardian.com/us-news/2016/sep/23/ahmad-khan-rahami-pakistan-taliban-new-york-bombing-terrorism',
         'http://www.theatlantic.com/entertainment/archive/2016/09/national-museum-of-african-american-history-and-culture-smithsonian/501356/',
         'https://bostonreview.net/us/colin-dayan-on-ice'
       ],
-      '3': [
+      '3%Wendy': [
         'http://www.economist.com/news/finance-economics/21707719-government-claims-publishing-report-would-undermine-efforts-retrieve',
         'http://www.motherjones.com/politics/2016/09/donald-trumps-appeal-ohios-rust-belt-isnt-just-about-economy',
         'https://www.wired.com/2016/09/whats-inside-blue-ballpoint-pen-ink/'
       ]
     },
     recommendedLinks: {
-      '2': [
+      '2%Jordan': [
         'http://qz.com/787633/farc-gathers-for-its-last-conference-as-a-guerrilla-after-50-years-of-war-to-vote-on-peace/',
         'http://www.npr.org/sections/thetwo-way/2016/09/23/495200112/witness-video-emerges-from-scene-of-charlotte-police-shooting-of-keith-scott',
         'http://consc.net/papers/spatial.pdf',
       ],
-      '3': [
+      '3%Wendy': [
         'http://www.abc.net.au/news/2016-09-23/federal-labor-hints-at-possible-plebiscite-compromise/7872550',
         'http://www.nytimes.com/2016/08/05/opinion/campaign-stops/i-ran-the-cia-now-im-endorsing-hillary-clinton.html?mwrsm=Facebook&_r=0',
         'http://thehill.com/blogs/in-the-know/in-the-know/297430-omarosa-trumps-haters-will-be-forced-to-bow-down-to-him'
--- fetch.js
+++ fetch.js
@@ -17,42 +17,48 @@ axios({
   for (key in ownLinks) {
     var userid = key.split('%')[0];
     var username = key.split('%')[1];
-    for (var i = 0; i < ownLinks[key].length; i++) {
-      var url = ownLinks[key][i];
-      get(url)
-      .then((res) => {
-        var $ = cheerio.load(res.data);
-        var title = $('title').text() //.replace(/[\s|]/g, '');
-        // console.log(title);
-        if (userid === id) {
-          var path = `Acorns/Me/Mine/${title}.html`;
-        } else {
-          var dir = `Acorns/${username}/`;
-          var path = `${dir}/${title}.html`;  
-          if (!fs.existsSync()) {
-            fs.mkdirSync(dir)           
-          }
-        }
-        fs.write(res.data);
-
-        console.log('Hey');
-        fs.writeFile(path, res.data, (err) => {
-          if (err) {
-            console.log('Err, yo', err);
-          }
-        });
-      });
-    }
+    getLinks(username, userid, ownLinks, key);
   }
-
 })
 .catch((err) => {
   console.log(err);
 });
 
-function get(link) {
-  return axios({
-    method: 'get',
-    url: link
-  });
+function getLinks(username, userid, ownLinks, key) {
+  for (var i = 0; i < ownLinks[key].length; i++) {
+    var url = ownLinks[key][i];
+    axios ({
+      method: 'get',
+      url: url
+    })
+    .then((res) => {
+      var $ = cheerio.load(res.data);
+      var title = $('title').text() //.replace(/[\s|]/g, '');
+      // console.log(title);
+      if (userid === id) {
+        var path = `Acorns/Me/Mine/${title}.html`;
+      } else {
+        var dir = `Acorns/${username}/`;
+        var path = `${dir}/${title}.html`;  
+        if (!fs.existsSync()) {
+          fs.mkdirSync(dir)           
+        }
+      }
+      fs.writeFile(path, res.data, (err) => {
+        if (err) {
+          console.log('Err, yo', err);
+        }
+      });
+    });
+  }
 };
+
+// function get(link, username) {
+
+//    var user = username;
+
+//   return axios ({
+//     method: 'get',
+//     url: link
+//   });
+// };
