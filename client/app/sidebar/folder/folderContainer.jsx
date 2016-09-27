1a72297946b0cf9c933ace20c991919de48d2faf
Damien Rochford <djrochford@gmail.com>
2016-09-27 10:10:59 -0700

    Folders populating, names displaying semi-correctly, iFrames populating, little janky.

--- client/app/sidebar/folder/folderContainer.jsx
+++ client/app/sidebar/folder/folderContainer.jsx
@@ -4,7 +4,7 @@ import FolderPresentation from './folderPresentation.jsx'
 var FolderContainer = (props) => (
 
   <div onClick={() => (props.loadFolder(props.folder))}>
-    <FolderPresentation folder={props.folder} folderName={props.folderName}/>
+    <FolderPresentation folder={props.folder}/>
   </div>
 )
 
