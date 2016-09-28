777190ccaa833a7b49280a2a5d8d3db5e3296b52
mwongka <mikekawong@gmail.com>
2016-09-28 16:40:51 -0700

    refactor fetch() methods to accept DB API data

--- src/fetch.js
+++ src/fetch.js
@@ -2,37 +2,108 @@ var axios = require('axios');
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-var id = '1';
+var id = '10105564501516258'; //<== hard coded for now. We need to figure out how to get desktop user ID from DB
+var name = 'Michael Wong'; // need a way to log int to get these before hand!
 
 console.log('I ran!');
 
 module.exports.call = () => {
+  //Mike's ridiculous refactoring data code below... :/
   axios({
   method: 'get',
-  url: `http://localhost:4000/links/${id}`
+  url: `http://localhost:8888/links/${id}`
   // params: {
   //   userid: id
   // }
   })
   .then((res) => {
-    linkObject = res.data;
-    ownLinks = res.data.ownLinks
-    recommendedLinks = res.data.recommendedLinks;
-    for (key in ownLinks) {
-      var userid = key.split('%')[0];
-      var username = key.split('%')[1];
-      getLinks(username, userid, ownLinks, key, 'Mine');
-    }
-    for (key in recommendedLinks) {
-      var userid = key.split('%')[0];
-      var username = key.split('%')[1];
-      getLinks(username, userid, recommendedLinks, key, 'Recommended');
-    }
+    // console.log(res.data, 'here are the res ');
+    var linksObject = { 
+      ownLinks: {},
+      recommendedLinks: {},
+    };
+    
+    res.data.forEach((curr) => {
+      if(curr.assignee === id){
+        linksObject.ownLinks[`${id}%${name}`] = linksObject.ownLinks[`${id}%${name}`] || [];
+        linksObject.ownLinks[`${id}%${name}`].push(curr.url);
+      } else {
+        linksObject.recommendedLinks[`${curr.assignee}`] = linksObject.recommendedLinks[`${curr.assignee}%`] || [];
+        linksObject.recommendedLinks[`${curr.assignee}`].push(curr.url);
+      }
+    })
+    return {res: res, linksObject: linksObject};
+  })
+  .then((data) => {
+    axios.get(`http://localhost:8888/friends/${id}`)
+    .then((res2) => {
+      console.log(data.res.data, 'here is res2');
+
+      var friendsList = {};
+      var linksObject = data.linksObject;
+
+      res2.data.forEach((friend) => {
+        var friendId = friend.fbid;
+        var friendName = friend.fbname;
+        var friendLinks = friend.links.map((link) => {
+          return link.url
+        });
+        if(linksObject.recommendedLinks[friendId]){
+          linksObject.recommendedLinks[`${friendId}%${friendName}`] = linksObject.recommendedLinks[friendId];
+          delete linksObject.recommendedLinks[friendId];
+        }
+        friendsList[friendId] = friendName;
+        linksObject.ownLinks[`${friendId}%${friendName}`] = friendLinks;
+      });
+
+      console.log(linksObject, 'links Object');
+      
+      ownLinks = linksObject.ownLinks
+      recommendedLinks = linksObject.recommendedLinks;
+      for (key in ownLinks) {
+        var userid = key.split('%')[0];
+        var username = key.split('%')[1];
+        getLinks(username, userid, ownLinks, key, 'Mine');
+      }
+      for (key in recommendedLinks) {
+        var userid = key.split('%')[0];
+        var username = key.split('%')[1];
+        getLinks(username, userid, recommendedLinks, key, 'Recommended');
+      }
+
+    })
   })
   .catch((err) => {
     console.log(err);
   });
 };
+// DAMIEN JORDAN CODE BELOW 
+// axios({
+//   method: 'get',
+//   url: `http://localhost:4000/links/${id}`
+//   // params: {
+//   //   userid: id
+//   // }
+//   })
+//   .then((res) => {
+//     linkObject = res.data;
+//     ownLinks = res.data.ownLinks
+//     recommendedLinks = res.data.recommendedLinks;
+//     for (key in ownLinks) {
+//       var userid = key.split('%')[0];
+//       var username = key.split('%')[1];
+//       getLinks(username, userid, ownLinks, key, 'Mine');
+//     }
+//     for (key in recommendedLinks) {
+//       var userid = key.split('%')[0];
+//       var username = key.split('%')[1];
+//       getLinks(username, userid, recommendedLinks, key, 'Recommended');
+//     }
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// };
 
 
 function getLinks(username, userid, links, key, type) {
