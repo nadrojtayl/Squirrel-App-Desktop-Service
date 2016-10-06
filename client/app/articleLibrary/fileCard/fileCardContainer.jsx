19aecedf764e8b8f7748e57f6009fc4fb4879c12
Damien Rochford <djrochford@gmail.com>
2016-10-06 11:53:02 -0700

    Working, slightly styled version.

--- client/app/articleLibrary/fileCard/fileCardContainer.jsx
+++ client/app/articleLibrary/fileCard/fileCardContainer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 class FileCardContainer extends React.Component {
 
   constructor(props) {
@@ -31,7 +29,6 @@ class FileCardContainer extends React.Component {
 
     var win = new BrowserWindow({ width: 800, height: 600 });
     console.log('NEW howdy')
-    console.log('topleveldir',topleveldir)
     console.log('total','file://' + url + '/article.html')
     var toload = 'file://' + url + '/article.html';
 
@@ -40,12 +37,27 @@ class FileCardContainer extends React.Component {
   }
 
   render() {
-    var image = fs.readdirSync(this.props.path)[10];
+    var regexFinal = /[^\/]+$/
+    var regexAfterApp = /app.*app\/([\s\S]+)$/
+    var title = regexFinal.exec(this.props.path)[0];
+    var imageFolder = __dirname + '/' + regexAfterApp.exec(this.props.path)[1] + '/images/';
+    console.log('imageFolder', imageFolder);
+    // console.log('this.props.path', this.props.path);
+    // console.log(title);
+    try {
+      var image = `Stash/Me/Mine/${title}/images/${fs.readdirSync(imageFolder)[10]}`;
+      console.log(image);
+    } catch (err) {
+      console.log(err);
+      var image = 'client/assets/default.png';
+    }
+    // console.log('dirname', __dirname);
+    // console.log(image);
     return (
       <div className = "articleBox" onClick={() => this.expand.bind(this)(this.props.path)} draggable="true" onDragStart={function(event){drag(event)}}>
         <img className="articleImage" src={image}/>
         <div className="articleTitle">
-          <h3> {this.props.path.slice(findsecondslash(this.props.path).bind(this)).replace('.html', '')}</h3>
+          <h3> {title}</h3>
         </div>
       </div>
     );
